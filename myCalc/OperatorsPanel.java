62a11c089be58cf3121ec3f71172a0559f70b688
Setzo <setzo94@gmail.com>
2015-01-10 21:30:05 +0100

    Update OperatorsPanel.java

--- myCalc/OperatorsPanel.java
+++ myCalc/OperatorsPanel.java
@@ -1,114 +1,52 @@
 package myCalc.gui;
 
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.BorderLayout;
+import java.awt.Font;
 
-import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
-import myCalc.interfaces.CalculationListener;
+public class TextPanel extends JPanel {
 
-public class OperatorsPanel extends JPanel implements ActionListener{
+	private static final long serialVersionUID = 5307792467600369271L;
 	
-	private static final long serialVersionUID = -2056623716068129959L;
-
-	private CalculationListener calcListener;
+	private JTextArea textArea;
 	
-	private JButton add;
-	private JButton substract;
-	private JButton multiply;
-	private JButton divide;
-	private JButton mod;
-	private JButton minus;
+	public TextPanel() {
+		/*
+		Dimension dim = getPreferredSize();
+		dim.height = 83;
+		dim.width = 300;
+		setPreferredSize(dim);
+		*/
+		
+		textArea = new JTextArea();
+		setLayout(new BorderLayout());
 	
-	public OperatorsPanel() {
+		textArea.setEditable(false);
 		
-		add 		= new JButton("+");
-		substract	= new JButton("-");
-		multiply	= new JButton("*");
-		divide		= new JButton("/");
-		mod 		= new JButton("%");
-		minus 		= new JButton("!");
+		Font font = textArea.getFont();
+		float size = font.getSize() + 5.0f;			//TODO changeable
+		textArea.setFont(font.deriveFont(size));
 		
-		add.addActionListener(this);
-		substract.addActionListener(this);
-		multiply.addActionListener(this);
-		divide.addActionListener(this);
-		mod.addActionListener(this);
-		minus.addActionListener(this);
-		
-		setLayout(new GridBagLayout());
-		GridBagConstraints gc = new GridBagConstraints();
-
-		gc.weightx = 0;
-		gc.weighty = 0;
-		gc.fill = GridBagConstraints.NONE;
-		
-		/////////ROW 1
-		gc.gridx = 0;
-		gc.gridy = 0;
-		gc.insets = new Insets(-26, 0, 0, 0);
-		add(add, gc);
-		
-		gc.gridx = 1;
-		gc.gridy = 0;
-		gc.insets = new Insets(-26, 8, 0, 0);
-		add(substract, gc);
-		
-		/////////ROW 2
-		gc.gridx = 0;
-		gc.gridy = 1;
-		gc.insets = new Insets(0, -2, 0, 0);
-		add(multiply, gc);
-		
-		gc.gridx = 1;
-		gc.gridy = 1;
-		gc.insets = new Insets(0, 9, 0, 0);
-		add(divide, gc);
-		
-		/////////ROW 3
-		gc.gridx = 0;
-		gc.gridy = 2;
-		gc.insets = new Insets(0, 1, 0, 0);
-		add(mod, gc);
-		
-		gc.gridx = 1;
-		gc.gridy = 2;
-		gc.insets = new Insets(0, 9, 0, 0);
-		add(minus, gc);
-	}
-
-	public void actionPerformed(ActionEvent e) {
-		
-		JButton clicked = (JButton)e.getSource();
-		
-		if (calcListener != null) {
-			
-			if (clicked == add) {
-				calcListener.encodedCalc('+', true);
-				
-			} else if (clicked == substract) {
-				calcListener.encodedCalc('-', true);
-				
-			} else if (clicked == multiply) {
-				calcListener.encodedCalc('*', true);
-				
-			} else if (clicked == divide) {
-				calcListener.encodedCalc('/', true);
-				
-			} else if (clicked == mod) {
-				calcListener.encodedCalc('%', true);
-				
-			} else if (clicked == minus) {
-				calcListener.encodedCalc('!', true);
-			}
-		}
+		add(new JScrollPane(textArea), BorderLayout.CENTER);
 	}
 	
-	public void setCalculationListener(CalculationListener listener) {
-		this.calcListener = listener;
+	public void appendText(Character c) {
+		textArea.append(c.toString());
+	}
+	
+	public void clrScr() {
+		textArea.setText(null);
+	}
+	
+	public void deleteLastCharacter() {
+
+		StringBuilder sb = new StringBuilder();
+		sb.append(textArea.getText());
+		sb.deleteCharAt(sb.length()-1);
+		textArea.setText(sb.toString());
+		sb.delete(0, sb.length());
 	}
 }
