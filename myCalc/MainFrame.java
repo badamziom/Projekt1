8f6b8908e2310e7f30dcd61996b343829828745e
Setzo <setzo94@gmail.com>
2015-01-10 21:29:05 +0100

    Update MainFrame.java

--- myCalc/MainFrame.java
+++ myCalc/MainFrame.java
@@ -1,16 +1,23 @@
-package myCalc;
+package myCalc.gui;
 
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.math.BigDecimal;
+import java.util.LinkedList;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.UIManager;
 import javax.swing.UIManager.LookAndFeelInfo;
 
+import myCalc.calculations.DoWork;
+import myCalc.interfaces.CalculationListener;
+
 public class MainFrame extends JFrame{
+	
+	private static final long serialVersionUID = -7220987384328102003L;
 	
 	private TextPanel calcArea;
 	private NumberPanel numberButtons;
@@ -19,7 +26,21 @@ public class MainFrame extends JFrame{
 	private OperatorsPanel operatorsPanel;
 	private JButton submitButton;
 	
-	private  StringBuilder sb;
+	private StringBuilder sb;
+	private StringBuilder sb2;
+	
+	private BigDecimal currentResult;
+	private BigDecimal currentResult2;
+	private LinkedList<BigDecimal> numList;
+	
+	private char nextOperator;
+	private char currentOperator;
+	
+	private boolean wasOL1;
+	private boolean wasOL2;
+	private boolean firstNum;
+	
+	private int listIterator;
 
 	public MainFrame(String title) {
 		
@@ -32,7 +53,19 @@ public class MainFrame extends JFrame{
 		operatorsPanel = new OperatorsPanel();
 		submitButton = new JButton("Submit");
 		
+		currentResult = BigDecimal.ZERO;
+		currentResult2 = BigDecimal.ZERO;
+		
+		numList = new LinkedList<BigDecimal>();
+		
 		sb = new StringBuilder();
+		sb2 = new StringBuilder();
+		
+		wasOL1 = false;
+		wasOL2 = false;
+		firstNum = true;
+		
+		listIterator = 1;
 		
 		try {
 		    for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
@@ -46,27 +79,100 @@ public class MainFrame extends JFrame{
 		}
 		
 		operatorsPanel.setCalculationListener(new CalculationListener() {
-			public void encodedCalc(char calculation) {
-				calcArea.appendText(calculation);
-				sb.append(calculation);
+			public void encodedCalc(char calculation, boolean wasOperatorLast) {
+				
+				sb2.delete(0, sb2.length());
+				
+				if(wasOL1) {
+					wasOL2 = true;
+				}
+				if(wasOperatorLast) {
+					wasOL1 = true;
+				}
+				if(wasOL1 && !wasOL2) {
+					calcArea.appendText(calculation);
+					sb.append(calculation);
+				}
+				if(wasOL1 && wasOL2) {
+					calcArea.deleteLastCharacter();
+					sb.deleteCharAt(sb.length()-1);
+					calcArea.appendText(calculation);
+					sb.append(calculation);
+				}
+				
+				if(!firstNum) {
+					
+					DoWork dw = new DoWork();
+					currentOperator = nextOperator;
+					
+					nextOperator = sb.charAt(sb.length() - 1);
+					sb.deleteCharAt(sb.length() - 1);
+
+					numList.add(new BigDecimal(sb.toString()));
+					sb.delete(0, sb.length());
+					
+					currentResult = dw.calculate(currentResult, numList.get(listIterator), currentOperator);
+					listIterator++;
+					
+					resultArea.clrScr();
+					resultArea.appendText(currentResult.toString());
+				}
+				else {
+					nextOperator = sb.charAt(sb.length() - 1);
+					sb.deleteCharAt(sb.length() - 1);
+
+					numList.add(new BigDecimal(sb.toString()));
+					
+					currentResult = new BigDecimal(sb.toString());
+					sb.delete(0, sb.length());
+					firstNum = false;
+				}
 			}
 		});
 		
 		numberButtons.setCalculationListener(new CalculationListener() {
-			public void encodedCalc(char calculation) {
+			public void encodedCalc(char calculation, boolean wasOperatorLast) {
+				
+				if(!wasOperatorLast) {
+					wasOL1 = false;
+					wasOL2 = false;
+				}
+				
 				calcArea.appendText(calculation);
 				sb.append(calculation);
+				sb2.append(calculation);
 				
+				if(!firstNum) {
+					
+					DoWork dw = new DoWork();
+					
+					currentResult2 = dw.calculate(currentResult, new BigDecimal(sb2.toString()), nextOperator);
+					
+					resultArea.clrScr();
+					resultArea.appendText(currentResult2.toString());
+				}
+				else {
+					resultArea.clrScr();
+					resultArea.appendText(sb.toString());
+				}
+
 			}
 		});
 		
 		submitButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				
 				calcArea.clrScr();
-				resultArea.clrScr();
-				DoWork dw = new DoWork();
-				resultArea.appendText(dw.calculate(sb.toString()).toString());
+				numList.removeAll(numList);
+				firstNum = true;
+				wasOL1 = false;
+				wasOL2 = false;
+				listIterator = 1;
+				currentResult = BigDecimal.ZERO;
+				currentResult2 = BigDecimal.ZERO;
 				sb.delete(0, sb.length());
+				sb2.delete(0, sb2.length());
+				//resultArea.clrScr();
 			}
 		});
 		
