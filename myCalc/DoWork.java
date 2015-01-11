2a8d170c575233a1fb18c85b7f56a1ec1cb00e8f
unknown <badamziom@o2.pl>
2015-01-11 22:59:52 +0100

    Modyfied layout

--- myCalc/DoWork.java
+++ myCalc/DoWork.java
@@ -7,93 +7,93 @@ import java.util.LinkedList;
 import java.util.Set;
 
 public class DoWork {
-	
-	private LinkedList<Double> numbers;
-	private LinkedList<Character> operators;
-	
-	private Set<Character> numSet;
-	private Set<Character> opsSet;
-	
-	public DoWork() {
-		
-		numbers = new LinkedList<Double>();
-		operators = new LinkedList<Character>();
-		
-		numSet = new HashSet<Character>();
-		numSet.add('1');
-		numSet.add('2');
-		numSet.add('3');
-		numSet.add('4');
-		numSet.add('5');
-		numSet.add('6');
-		numSet.add('7');
-		numSet.add('8');
-		numSet.add('9');
-		numSet.add('0');
-		numSet.add('.');
-		
-		opsSet = new HashSet<Character>();
-		opsSet.add('+');
-		opsSet.add('-');
-		opsSet.add('*');
-		opsSet.add('/');
-		opsSet.add('%');
-		opsSet.add('!');
-	}
-	
-	public BigDecimal calculate(String chain) {
-		
-		BigDecimal result = BigDecimal.ZERO;
-		StringBuilder sb = new StringBuilder();
-		int x = 1;
-		
-		for(int i = 0; i < chain.length(); i++) {
-			
-			if(numSet.contains(chain.charAt(i))) {
-				sb.append(chain.charAt(i));
-				if(i + 1 == chain.length()) {
-					numbers.add(Double.parseDouble(sb.toString()));
-					sb.delete(0, sb.length());
-				}
-			} else {
-				numbers.add(Double.parseDouble(sb.toString()));
-				operators.add(chain.charAt(i));
-				sb.delete(0, sb.length());
-			}
-		}
-		
-		result = BigDecimal.valueOf(numbers.get(0));
-		Boolean negate = false;
-		
-		for(char op : operators) {
-			switch(op) {
-				case '+': 
-					result = result.add(BigDecimal.valueOf(numbers.get(x)));
-					break;
-				case '-':
-					result = result.subtract(BigDecimal.valueOf(numbers.get(x)));
-					break;
-				case '*':
-					result = result.multiply(BigDecimal.valueOf(numbers.get(x)), MathContext.DECIMAL64);
-					break;
-				case '/':
-					result = result.divide(BigDecimal.valueOf(numbers.get(x)), MathContext.DECIMAL64);
-					break;
-				case '%':
-					result = result.remainder(BigDecimal.valueOf(numbers.get(x)), MathContext.DECIMAL64);
-					break;
-				case '!':
-					negate = !negate;
-					break;
-				default :
-					break;
-			}
-			x++;
-		}
-		if(negate) {
-			result.negate();
-		}
-		result.stripTrailingZeros();
-		return result;
-	}
+
+    private LinkedList<Double> numbers;
+    private LinkedList<Character> operators;
+
+    private Set<Character> numSet;
+    private Set<Character> opsSet;
+
+    public DoWork() {
+
+        numbers = new LinkedList<Double>();
+        operators = new LinkedList<Character>();
+
+        numSet = new HashSet<Character>();
+        numSet.add('1');
+        numSet.add('2');
+        numSet.add('3');
+        numSet.add('4');
+        numSet.add('5');
+        numSet.add('6');
+        numSet.add('7');
+        numSet.add('8');
+        numSet.add('9');
+        numSet.add('0');
+        numSet.add('.');
+
+        opsSet = new HashSet<Character>();
+        opsSet.add('+');
+        opsSet.add('-');
+        opsSet.add('*');
+        opsSet.add('/');
+        opsSet.add('%');
+        opsSet.add('!');
+    }
+
+    public BigDecimal calculate(String chain) {
+
+        BigDecimal result = BigDecimal.ZERO;
+        StringBuilder sb = new StringBuilder();
+        int x = 1;
+
+        for (int i = 0; i < chain.length(); i++) {
+
+            if (numSet.contains(chain.charAt(i))) {
+                sb.append(chain.charAt(i));
+                if (i + 1 == chain.length()) {
+                    numbers.add(Double.parseDouble(sb.toString()));
+                    sb.delete(0, sb.length());
+                }
+            } else {
+                numbers.add(Double.parseDouble(sb.toString()));
+                operators.add(chain.charAt(i));
+                sb.delete(0, sb.length());
+            }
+        }
+
+        result = BigDecimal.valueOf(numbers.get(0));
+        Boolean negate = false;
+
+        for (char op : operators) {
+            switch (op) {
+                case '+':
+                    result = result.add(BigDecimal.valueOf(numbers.get(x)));
+                    break;
+                case '-':
+                    result = result.subtract(BigDecimal.valueOf(numbers.get(x)));
+                    break;
+                case '*':
+                    result = result.multiply(BigDecimal.valueOf(numbers.get(x)), MathContext.DECIMAL64);
+                    break;
+                case '/':
+                    result = result.divide(BigDecimal.valueOf(numbers.get(x)), MathContext.DECIMAL64);
+                    break;
+                case '%':
+                    result = result.remainder(BigDecimal.valueOf(numbers.get(x)), MathContext.DECIMAL64);
+                    break;
+                case '!':
+                    negate = !negate;
+                    break;
+                default:
+                    break;
+            }
+            x++;
+        }
+        if (negate) {
+            result.negate();
+        }
+        result.stripTrailingZeros();
+        return result;
+    }
 }
--- myCalc/MainFrame.java
+++ myCalc/MainFrame.java
@@ -10,112 +10,112 @@ import javax.swing.JFrame;
 import javax.swing.UIManager;
 import javax.swing.UIManager.LookAndFeelInfo;
 
-public class MainFrame extends JFrame{
-	
-	private TextPanel calcArea;
-	private NumberPanel numberButtons;
-	private ResultPanel resultArea;
-	private OptionsPanel options;
-	private OperatorsPanel operatorsPanel;
-	private JButton submitButton;
-	
-	private  StringBuilder sb;
+public class MainFrame extends JFrame {
 
-	public MainFrame(String title) {
-		
-		super(title);
-		
-		calcArea = new TextPanel();
-		numberButtons = new NumberPanel();
-		resultArea = new ResultPanel();
-		options = new OptionsPanel();
-		operatorsPanel = new OperatorsPanel();
-		submitButton = new JButton("Submit");
-		
-		sb = new StringBuilder();
-		
-		try {
-		    for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-		        if ("Nimbus".equals(info.getName())) {
-		            UIManager.setLookAndFeel(info.getClassName());
-		            break;
-		        }
-		    }
-		} catch (Exception e) {
-			System.out.println("Nimbus L&F not found");
-		}
-		
-		operatorsPanel.setCalculationListener(new CalculationListener() {
-			public void encodedCalc(char calculation) {
-				calcArea.appendText(calculation);
-				sb.append(calculation);
-			}
-		});
-		
-		numberButtons.setCalculationListener(new CalculationListener() {
-			public void encodedCalc(char calculation) {
-				calcArea.appendText(calculation);
-				sb.append(calculation);
-				
-			}
-		});
-		
-		submitButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				calcArea.clrScr();
-				resultArea.clrScr();
-				DoWork dw = new DoWork();
-				resultArea.appendText(dw.calculate(sb.toString()).toString());
-				sb.delete(0, sb.length());
-			}
-		});
-		
-		setLayout(new GridBagLayout());
-		GridBagConstraints gc = new GridBagConstraints();
-		
-		gc.weightx = 1;
-		gc.weighty = 1;
-		gc.fill = GridBagConstraints.HORIZONTAL;
-		
-		/////////ROW 1
-		gc.gridwidth = 2;
-		gc.gridx = 0;
-		gc.gridy = 0; 
-		gc.ipady = 83;
-		gc.anchor = GridBagConstraints.NORTH;
-		add(calcArea, gc);
-		
-		/////////ROW 2
-		gc.ipady = 5;
-		gc.gridx = 0;
-		gc.gridy = 1;
-		add(resultArea, gc);
-		
-		/////////ROW 3
-		gc.gridx = 0;
-		gc.gridy = 2;
-		add(submitButton, gc);
-		
-		/////////ROW 4
-		gc.ipady = 0;
-		gc.gridwidth = 1;
-		gc.fill = GridBagConstraints.BOTH;
-		gc.gridx = 0;
-		gc.gridy = 3;
-		add(numberButtons, gc);
-		
-		gc.gridx = 1;
-		gc.gridy = 3;
-		add(operatorsPanel, gc);
-		
-		/////////ROW 5
-		gc.gridwidth = 2;
-		gc.gridx = 0;
-		gc.gridy = 4;
-		add(options, gc);
-		
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(300, 400);
-		setVisible(true);
-	}
+    private TextPanel calcArea;
+    private NumberPanel numberButtons;
+    private ResultPanel resultArea;
+    private OptionsPanel options;
+    private OperatorsPanel operatorsPanel;
+    private JButton submitButton;
+
+    private StringBuilder sb;
+
+    public MainFrame(String title) {
+
+        super(title);
+
+        calcArea = new TextPanel();
+        numberButtons = new NumberPanel();
+        resultArea = new ResultPanel();
+        options = new OptionsPanel();
+        operatorsPanel = new OperatorsPanel();
+        submitButton = new JButton("Submit");
+
+        sb = new StringBuilder();
+
+        try {
+            for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Nimbus L&F not found");
+        }
+
+        operatorsPanel.setCalculationListener(new CalculationListener() {
+            public void encodedCalc(char calculation) {
+                calcArea.appendText(calculation);
+                sb.append(calculation);
+            }
+        });
+
+        numberButtons.setCalculationListener(new CalculationListener() {
+            public void encodedCalc(char calculation) {
+                calcArea.appendText(calculation);
+                sb.append(calculation);
+
+            }
+        });
+
+        submitButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                calcArea.clrScr();
+                resultArea.clrScr();
+                DoWork dw = new DoWork();
+                resultArea.appendText(dw.calculate(sb.toString()).toString());
+                sb.delete(0, sb.length());
+            }
+        });
+
+        setLayout(new GridBagLayout());
+        GridBagConstraints gc = new GridBagConstraints();
+
+        gc.weightx = 1;
+        gc.weighty = 1;
+        gc.fill = GridBagConstraints.HORIZONTAL;
+
+        /////////ROW 1
+        gc.gridwidth = 2;
+        gc.gridx = 0;
+        gc.gridy = 0;
+        gc.ipady = 30;
+        gc.anchor = GridBagConstraints.NORTH;
+        add(calcArea, gc);
+
+        /////////ROW 2
+        gc.ipady = 5;
+        gc.gridx = 0;
+        gc.gridy = 1;
+        add(resultArea, gc);
+
+        /////////ROW 3
+        gc.gridx = 0;
+        gc.gridy = 2;
+        add(submitButton, gc);
+
+        /////////ROW 4
+        gc.ipady = 0;
+        gc.gridwidth = 1;
+        gc.fill = GridBagConstraints.BOTH;
+        gc.gridx = 0;
+        gc.gridy = 3;
+        add(numberButtons, gc);
+
+        gc.gridx = 1;
+        gc.gridy = 3;
+        add(operatorsPanel, gc);
+
+        /////////ROW 5
+        gc.gridwidth = 2;
+        gc.gridx = 0;
+        gc.gridy = 4;
+        add(options, gc);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(300, 400);
+        setVisible(true);
+    }
 }
