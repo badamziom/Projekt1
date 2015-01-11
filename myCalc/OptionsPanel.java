c7acfb28ffe4c34744c4cdeeb61f23c935d619b3
Setzo <setzo94@gmail.com>
2015-01-11 01:51:46 +0100

    Update OptionsPanel.java

--- myCalc/OptionsPanel.java
+++ myCalc/OptionsPanel.java
@@ -2,6 +2,9 @@ package myCalc.gui;
 
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -16,23 +19,30 @@ public class OptionsPanel extends JPanel implements ActionListener{
 	
 	private StringListener strListener;
 	private JButton save;
+	private JButton copy;
 	
 	public OptionsPanel() {
 		
 		save = new JButton("Save");
+		copy = new JButton("Copy to clipboard");
 		
 		save.addActionListener(this);
+		copy.addActionListener(this);
 		
 		setLayout(new GridBagLayout());
 		GridBagConstraints gc = new GridBagConstraints();
 		
 		gc.weightx = 1;
 		gc.weighty = 1;
-		gc.fill = GridBagConstraints.NONE;
+		gc.fill = GridBagConstraints.BOTH;
 		gc.gridx = 0;
 		gc.gridy = 0;
 		gc.anchor = GridBagConstraints.SOUTH;
-		add(save, gc);	
+		add(save, gc);
+		
+		gc.gridx = 1;
+		gc.gridy = 0;
+		add(copy, gc);
 	}
 	
 	public void setStringListener(StringListener listener) {
@@ -44,9 +54,18 @@ public class OptionsPanel extends JPanel implements ActionListener{
 		
 		JButton clicked = (JButton)e.getSource();
 		
-		if(this.strListener != null) {
+		if(clicked != null) {
 			if(clicked == save) {
-				this.strListener.initiate(true);
+				if (this.strListener != null) {
+					if (clicked == save) {
+						this.strListener.initiate(true);
+					}
+				}
+				
+			} else if(clicked == copy) {
+				StringSelection strSel = new StringSelection(this.strListener.getText());
+				Clipboard clp = Toolkit.getDefaultToolkit().getSystemClipboard();
+				clp.setContents(strSel, null);
 			}
 		}
 	}
