862eb4786decec424fcc584df8da11e0a73d339e
Setzo <setzo94@gmail.com>
2015-01-13 19:10:29 +0100

    Delete StringListener.java

--- myCalc/StringListener.java
+++
@@ -1,9 +0,0 @@
-package myCalc.interfaces;
-
-public interface StringListener {
-	public void initiate(boolean start);
-	public void saveToFile(String text);
-	public void setText(String text);
-	public boolean isItReady();
-	public String getText();
-}
